909a41d25c5baeaa9b8729fb5c55ce865fc69c9a
ZeyadMousa1 <zeyadmousa1919@gmail.com>
2025-06-20 01:39:53 +0300

    fix background

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Header from './src/assets/Images/Header/Header.jpg'
-import { backInOut } from 'motion/react'
 
 // https://vite.dev/config/
 export default defineConfig({
