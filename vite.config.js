4eb0a6f3533e1f5bbba06373ff9422529b758873
ZeyadMousa1 <zeyadmousa1919@gmail.com>
2025-06-20 01:33:43 +0300

    fix background

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Header from './src/assets/Images/Header/Header.jpg'
+import { backInOut } from 'motion/react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+const HeadStyle = {
+  backgrounImage: `url(${Header})`,
+  backgrounSize: 'cover'
+}
